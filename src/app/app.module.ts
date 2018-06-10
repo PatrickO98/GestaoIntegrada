0a1826482f95da9b6a7eb53118af1224f2aa2f83
PatrickO98 <patrick.oliveira98@live.com>
2018-06-10 00:22:21 -0300

    Adicionado Componente "Colaboradores"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ColaboradoresComponent } from './colaboradores/colaboradores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColaboradoresComponent
   ],
   imports: [
     BrowserModule
